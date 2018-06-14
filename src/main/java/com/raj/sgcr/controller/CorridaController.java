e77f5899e538492222319f1f1f0b6cb48ef4db49
Rafael S. L <rafael.souza.lana@gmail.com>
2018-06-14 10:31:07 -0300

    Modificado Corrida para novo padrão e adicionado bootstrap 4

---
+++ src/main/java/com/raj/sgcr/controller/CorridaController.java
@@ -0,0 +1,81 @@
+package com.raj.sgcr.controller;
+
+import com.raj.sgcr.domain.model.Corrida;
+import com.raj.sgcr.domain.repository.CorridaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping(value = "corridas")
+public class CorridaController {
+
+    @Autowired
+    private CorridaRepository corridaRepository;
+
+    @GetMapping(value = "")
+    public String corridas(Model model){
+        model.addAttribute("operacao", "listar");
+        model.addAttribute("title", "Lista de corridas");
+        model.addAttribute("corridas", corridaRepository.findAll());
+        return "corrida/corridas";
+    }
+
+    @GetMapping(value = "add")
+    public String getCorridasAdd(Model model){
+        model.addAttribute("operacao", "adicionar");
+        model.addAttribute("title", "Adicionar corrida");
+        return "corrida/add";
+    }
+
+    @PostMapping(value = "add")
+    public String postCorridasAdd(Model model, @ModelAttribute Corrida corrida){
+        model.addAttribute("title", "Adicionar corrida");
+        corridaRepository.save(corrida);
+        return "redirect:";
+    }
+
+    @GetMapping(value = "edit/{id}")
+    public String getCorridaEdit(Model model, @PathVariable Long id) {
+        model.addAttribute("operacao", "editar");
+        model.addAttribute("title", "Editar corrida");
+        Optional<Corrida> corrida = corridaRepository.findById(id);
+        if (corrida.isPresent()){
+            model.addAttribute("corrida", corrida.get());
+        }
+        return "corrida/edit";
+    }
+
+    @PostMapping(value = "edit/{id}")
+    public String postCorridaEdit(@ModelAttribute Corrida corrida, Model model,
+                                  @PathVariable Long id) throws Exception {
+        if (id.equals(corrida.getId())) {
+            corridaRepository.save(corrida);
+        } else {
+            model.addAttribute("error", "Dados incorretos");
+        }
+        return "redirect:/corridas";
+    }
+
+    @GetMapping(value = "delete/{id}")
+    public String getCorridaDelete(Model model, @PathVariable Long id) {
+        model.addAttribute("operacao", "deletar");
+        model.addAttribute("title", "Excluir corrida");
+        Optional<Corrida> corrida = corridaRepository.findById(id);
+        if (corrida.isPresent()) {
+            model.addAttribute("corrida", corrida.get());
+        }
+
+        return "corrida/delete";
+    }
+
+    @PostMapping(value = "delete/{id}")
+    public String postCorridaDelete(@PathVariable Long id, @ModelAttribute Corrida corrida) {
+        corridaRepository.delete(corrida);
+        return "redirect:/corridas";
+    }
+
+}
---
+++ src/main/java/com/raj/sgcr/domain/model/Corrida.java
@@ -0,0 +1,40 @@
+package com.raj.sgcr.domain.model;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Accessors(chain = true)
+public class Corrida implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long serialVersionUID = 1L;
+    private String nome;
+    private Date horario;
+    private Date data;
+    private Integer maxPessoa;
+    private String banner;
+    private String descricao;
+    private String cep;
+    private String estado;
+    private String cidade;
+    private String bairro;
+    private String logradouro;
+    private String numero;
+    private Integer edicao;
+    private Integer ativo;
+    private Date dataInicioRetiradaKit;
+    private Date dataFinalRetiradaKit;
+    //@ManyToOne
+    //private Organizador organizador;
+}
+
