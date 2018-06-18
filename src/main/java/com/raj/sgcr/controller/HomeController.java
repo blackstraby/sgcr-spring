1986891297b8738d5a3d5504c1f8177954d360ff
Jonas Gomes <jj.ti@hotmail.com>
2018-06-18 17:12:44 -0300

    Pagina Inicial Dashboard

--- src/main/java/com/raj/sgcr/controller/HomeController.java
+++ src/main/java/com/raj/sgcr/controller/HomeController.java
@@ -1,27 +1,46 @@
 package com.raj.sgcr.controller;
 
+import com.raj.sgcr.domain.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 @Controller
-
+@RequestMapping(value = "/")
 public class HomeController {
+    private CorridaRepository corridaRepository;
+    private KitRepository kitRepository;
+    private LoteRepository loteRepository;
+    private PercursoRepository percursoRepository;
 
+    @Autowired
+    public HomeController(CorridaRepository corridaRepository,
+                          KitRepository kitRepository,
+                          LoteRepository loteRepository,
+                          PercursoRepository percursoRepository    ) {
+        this.corridaRepository = corridaRepository;
+        this.kitRepository = kitRepository;
+        this.loteRepository = loteRepository;
+        this.percursoRepository = percursoRepository;
+    }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(Locale locale, Model model) {
+    @GetMapping(value = "")
+    public String corridas(Model model) {
+        model.addAttribute("dataAtual", new SimpleDateFormat("dd/MM/yyyy").format(new Date(System.currentTimeMillis())));
+        model.addAttribute("title", "Dashboard");
 
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        model.addAttribute("corridas", corridaRepository.findAll());
 
-        String formattedDate = dateFormat.format(date);
-
-        model.addAttribute("serverTime", formattedDate );
+        model.addAttribute("numCorridas", corridaRepository.count());
+        model.addAttribute("numKit", kitRepository.count());
+        model.addAttribute("numLote", loteRepository.count());
+        model.addAttribute("numPercurso", percursoRepository.count());
 
         return "home";
     }
