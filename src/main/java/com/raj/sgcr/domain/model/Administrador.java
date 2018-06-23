5b76f6183a6b45396ae74b62efbd140d8262fe51
Jonas Gomes <jj.ti@hotmail.com>
2018-06-23 08:50:52 -0300

    Report no administrador

--- src/main/java/com/raj/sgcr/domain/model/Administrador.java
+++ src/main/java/com/raj/sgcr/domain/model/Administrador.java
@@ -3,6 +3,7 @@ package com.raj.sgcr.domain.model;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -17,6 +18,7 @@ public class Administrador implements Serializable {
     @GeneratedValue
     private Long id;
     private String nome;
+    @Column(unique=true)
     private String email;
     private String senha;
 
