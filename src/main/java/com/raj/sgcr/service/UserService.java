21814c2656ef38d622070d0f43de6ecaa303979a
Rafael S. L <rafael.souza.lana@gmail.com>
2018-06-25 19:27:07 -0300

    Reestruturação

--- src/main/java/com/raj/sgcr/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.raj.sgcr.service;
-
-import com.raj.sgcr.domain.model.Usuario;
-
-public interface UserService {
-    public Usuario findUsuarioByEmail(String email);
-    public void saveUsuario(Usuario user);
-}
